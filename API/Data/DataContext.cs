43f275691d7e4d173515ac47aba82e9ab14b6adf
codeByNawin <win.nantawichai@gmail.com>
2025-01-17 15:45:02 +0700

    End of Section  2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using System;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
+
+    public static implicit operator ControllerContext(DataContext v)
+    {
+        throw new NotImplementedException();
+    }
+}
